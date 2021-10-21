1e3844eab45c787caa8f06e1e9843d65dff18197
rogertmalg <roger.malgueira@outlook.com>
2021-10-21 15:07:07 +0100

    test 3 not yet resolved, paused working for lab

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,3 +5,8 @@ def get_pet_shop_name(shop_name):
 
 def get_total_cash(shop_name):
     return shop_name["admin"]["total_cash"]
+
+# def add_or_remove_cash(shop_name, amount):
+#     return get_total_cash(shop_name) + amount
+
+# not yet resolved 
