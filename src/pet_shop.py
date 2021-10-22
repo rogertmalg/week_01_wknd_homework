f7e0631712ddb11a980a7188f382a09fecc24fdb
rogertmalg <roger.malgueira@outlook.com>
2021-10-22 15:42:11 +0100

    integration tests ok

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -32,26 +32,26 @@ def get_pets_by_breed(shop_name, breed_name):
     return pets_breed
 # this solves 8 and 9
 
-# def find_pet_by_name(shop_name, pet_name):
+# def find_pet_by_name(shop_name, shop_pet):
 #     found_pet = {}
 #     for pet in shop_name["pets"]:
-#         if pet["name"] == pet_name:
+#         if pet["name"] == shop_pet:
 #             found_pet.update(pet)
 
 #     return found_pet
 # Changed above funtion to function bellow, this solves 10 and 11
 
-def find_pet_by_name(shop_name, pet_name):
+def find_pet_by_name(shop_name, shop_pet):
     found_pet = None
     for pet in shop_name["pets"]:
-        if pet["name"] == pet_name:
+        if pet["name"] == shop_pet:
             found_pet = pet
 
     return found_pet
 
-def remove_pet_by_name(shop_name, pet_name):
+def remove_pet_by_name(shop_name, shop_pet):
     for pet in shop_name["pets"]:
-        if pet["name"] == pet_name:
+        if pet["name"] == shop_pet:
             shop_name["pets"].remove(pet)
 
 def add_pet_to_stock(shop_name, new_pet):
@@ -77,3 +77,29 @@ def customer_can_afford_pet(customer_index, new_pet):
         can_afford = True
     return can_afford
 # solves 18, 19 and 20 
+
+# Integration tests
+
+def sell_pet_to_customer(shop_name, shop_pet, customer_index):
+    if shop_pet == None:
+        print("We do not have this pet")
+
+    elif customer_index["cash"] >= shop_pet["price"]:
+    
+        amount = shop_pet["price"]
+        number_pets = 1
+        customer_index["pets"].append(shop_pet)
+        remove_pet_by_name(shop_name, shop_pet)
+        increase_pets_sold(shop_name, number_pets)
+        add_or_remove_cash(shop_name, amount)
+        remove_customer_cash(customer_index, amount)
+
+    else:
+        print("you cant afford this pet")
+
+# for the if and else statement, if I don't add an indented line under I get an error
+# I thought I would be able to use the if withtout anything happening if true
+# the work around was to print something after it, so I added the else as it also prints something 
+# when the customer cant afford, the else is not needed tho. 
+
+        
